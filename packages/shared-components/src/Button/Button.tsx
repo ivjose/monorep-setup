1e4010241743481241404b96bd1dc60401185513
outcrate <jsantos@outcrate.xyz>
2020-08-28 19:21:33 +0800

    Added lint option

--- packages/shared-components/src/Button/Button.tsx
+++ packages/shared-components/src/Button/Button.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
-/* eslint-disable react/display-name */
 import React from 'react';
 import { ButtonProps as MUIButtonProps } from '@material-ui/core';
 import MuiButton from '@material-ui/core/Button';
